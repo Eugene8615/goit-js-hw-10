aa20183f3cb16023053939c9ad82394930b44e20
Eugene8615 <reznichenko8615@gmail.com>
2023-01-29 17:27:51 +0200

    2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const inputField = document.getElementById('search-box');
+inputField.addEventListener('input', debounce(onGetRequest, DEBOUNCE_DELAY));
+
+const listCountry = document.querySelector('.country-list');
+const infoCountry = document.querySelector('.country-info');
+
+let name = '';
+
+async function onGetRequest() {
+  name = inputField.value.trim();
+
+  if (inputField.value === '') {
+    listCountry.innerHTML = '';
+    infoCountry.innerHTML = '';
+    return;
+  }
+
+  const countriesResponce = await fetchCountries(name);
+  listCountry.innerHTML = '';
+  infoCountry.innerHTML = '';
+
+  try {
+  if (countriesResponce.length > 10) {
+    Notify.info('Too many matches found. Please enter a more specific name.');
+  }
+
+  if (countriesResponce.length >= 2 && countriesResponce.length <= 10) {
+    const listCountriesMarkup = countriesResponce
+      .map(country => {
+        return `<li>
+            <img src='${country.flags.svg}' alt='Flag' width="80" height="40" />
+          <p>${country.name}</p>
+          </li>`;
+      })
+      .join('');}
+
+    listCountry.insertAdjacentHTML('beforeend', listCountriesMarkup);
+  }
+catch(e){
+  if (countriesResponce.length === 1) {
+    const countryMarkup = countriesResponce.map(country => {
+      return `<img src='${
+        country.flags.svg
+      }' alt='Flag' width="100" heigth"50" />
+          <h2>${country.name}</h2>
+          <p><span>Capital:</span>${country.capital}</p> 
+          <p><span>Population:</span>${country.population}</p> 
+          <p><span>Laguages:</span>${country.languages.map(
+            name => name.name
+          )}</p> `;
+    });
+
+    infoCountry.insertAdjacentHTML('beforeend', countryMarkup);
+  }
+} }
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,12 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+export async function fetchCountries(name) {
+  const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flags,languages`;
+
+  const response = await fetch(url);
+  const countries = await response.json();
+
+  if (response.ok === false) {
+    throw new Error (Notify.failure('Oops, there is no country with that name'));
+  }
+  return countries;
+}
